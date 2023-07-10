8295df87f8446d93a76215980e3d7a255c0312ed
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2023-07-11 02:06:14 +0530

    Portfolio Section Code redundancy fixed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== Toggle Skills ====================*/
 const skillsContent = document.getElementsByClassName("skills__content"),
   skillsHeader = document.querySelectorAll(".skills__header");
 
@@ -33,32 +33,6 @@ function opentab(tabname) {
   document.getElementById(tabname).classList.add("active-tab");
 }
 
-// <!-- -----------Portfolio filter script--------- -->
-
-      // var tabfilters = document.getElementsByClassName("list");
-      // var tabfiltercontents = document.getElementsByClassName(
-      //   "portfolio-container"
-      // );
-
-      // function openfilter(tabfilter) {
-      //   var selectedContainer = document.getElementById(tabfilter);
-
-      //   if (!selectedContainer.classList.contains("active-filter-selected")) {
-      //     // Filter is not active, toggle it on
-      //     for (var i = 0; i < tabfilters.length; i++) {
-      //       tabfilters[i].classList.remove("active-filter");
-      //     }
-      //     for (var j = 0; j < tabfiltercontents.length; j++) {
-      //       tabfiltercontents[j].classList.remove("active-filter-selected");
-      //       tabfiltercontents[j].style.display = "none";
-      //     }
-      //     event.currentTarget.classList.add("active-filter");
-      //     selectedContainer.classList.add("active-filter-selected");
-      //     selectedContainer.style.display = "flex";
-      //   }
-      // }
-
-
     // <!-- -----------Contact Me Form Script--------- -->
 
       var sidmenu = document.getElementById("sidemenu");
@@ -69,8 +43,6 @@ function opentab(tabname) {
       function closemenu() {
         sidmenu.style.right = "-200px";
       }
-
-
 
       const scriptURL =
         "https://script.google.com/macros/s/AKfycbxrdqUDIVns0po7HGyAyTslEofskNG6iec9AF109f2vGvkcfas6FDX1PmzYmumPd6SDzA/exec";
@@ -148,14 +120,14 @@ function opentab(tabname) {
     type(); // Start the typing animation
     
  
-  // --------------About Me Typing Animation----------
-  const typingText = `I am <Bhaskar Acharjee>, an aspiring <Electronics and Communication engineer> currently pursuing my <Bachelor of Technology> at <Jalpaiguri Government Engineering College>. With a strong passion for technology and design, I constantly strive to explore and enhance my skills in various domains.
-  My journey in the field of technology has equipped me with a wide range of technical skills. I am proficient in languages such as <Python>, <Java>, <C/C++>, <HTML/CSS>, and <SQL>. I have hands-on experience with developer tools like <VS Code>, <Android Studio>, <Jupyter Notebooks>, and <MATLAB>. Additionally, I have expertise in <Android app development>, <data analytics> using tools like <Power BI> and <Advanced Excel>, and <graphic design> using Canva, Figma, and Adobe software.
-  Beyond technical prowess, I have actively engaged in challenging projects that have honed my problem-solving abilities and attention to detail. I have contributed to the development of PixelCraft, an image editing tool built using the PIL library, allowing users to unleash their creativity through various filters and canvas effects. I have also created Corvas Notes, an Android application for digital note-taking, and an Attendance App for efficient management of student attendance. Driven by a curious and analytical mindset, I have engaged in research and published a paper on the design of a wideband microstrip patch antenna for 5G networks. Moreover, I have actively participated in leadership roles, serving as the Placement Coordinator for the Training and Placement Cell and Finance Manager at the Center For Innovation in my college.
-  With a strong foundation in both technical and soft skills, I am eager to contribute my knowledge, creativity, and dedication to making a meaningful impact in the field of technology. I am open to new opportunities and collaborations that allow me to continuously learn, grow, and contribute to innovative projects.`;
+    // --------------About Me Typing Animation----------
+    const typingText = `I am <Bhaskar Acharjee>, an aspiring <Electronics and Communication engineer> currently pursuing my <Bachelor of Technology> at <Jalpaiguri Government Engineering College>. With a strong passion for technology and design, I constantly strive to explore and enhance my skills in various domains.
+    My journey in the field of technology has equipped me with a wide range of technical skills. I am proficient in languages such as <Python>, <Java>, <C/C++>, <HTML/CSS>, and <SQL>. I have hands-on experience with developer tools like <VS Code>, <Android Studio>, <Jupyter Notebooks>, and <MATLAB>. Additionally, I have expertise in <Android app development>, <data analytics> using tools like <Power BI> and <Advanced Excel>, and <graphic design> using Canva, Figma, and Adobe software.
+    Beyond technical prowess, I have actively engaged in challenging projects that have honed my problem-solving abilities and attention to detail. I have contributed to the development of PixelCraft, an image editing tool built using the PIL library, allowing users to unleash their creativity through various filters and canvas effects. I have also created Corvas Notes, an Android application for digital note-taking, and an Attendance App for efficient management of student attendance. Driven by a curious and analytical mindset, I have engaged in research and published a paper on the design of a wideband microstrip patch antenna for 5G networks. Moreover, I have actively participated in leadership roles, serving as the Placement Coordinator for the Training and Placement Cell and Finance Manager at the Center For Innovation in my college.
+    With a strong foundation in both technical and soft skills, I am eager to contribute my knowledge, creativity, and dedication to making a meaningful impact in the field of technology. I am open to new opportunities and collaborations that allow me to continuously learn, grow, and contribute to innovative projects.`;
 
-  const staticText = `I am a highly motivated and diligent individual pursuing a Bachelor of Technology degree in <b>Electronics and Communication</b> at <b>Jalpaiguri Government Engineering College</b>. With a strong academic record and proficiency in programming languages like Java, Python, C/C++, and SQL, I have developed Android applications and also gained expertise in Data visualization. Additionally, I actively contribute to the Training and Placement Cell as a role of Placement Coordinator. 
-  <br/>With my strong educational background, technical skills, and leadership experiences, I am well-prepared to take on challenges and contribute effectively to any professional setting relevant with my work profile.`;
+    const staticText = `I am a highly motivated and diligent individual pursuing a Bachelor of Technology degree in <b>Electronics and Communication</b> at <b>Jalpaiguri Government Engineering College</b>. With a strong academic record and proficiency in programming languages like Java, Python, C/C++, and SQL, I have developed Android applications and also gained expertise in Data visualization. Additionally, I actively contribute to the Training and Placement Cell as a role of Placement Coordinator. 
+    <br/>With my strong educational background, technical skills, and leadership experiences, I am well-prepared to take on challenges and contribute effectively to any professional setting relevant with my work profile.`;
   
     const aboutParagraph = document.getElementById("about-paragraph");
     const staticParagraph = document.getElementById("static-paragraph");
@@ -206,31 +178,6 @@ function opentab(tabname) {
       aboutType(); // Start the typing animation
     });
 
-
-
-    // <!-- ----------Animate Skills-------- -->
-
-      function animateSkillBars() {
-        const skillBars = document.querySelectorAll(".skill-progress");
-
-        skillBars.forEach((skillBar) => {
-          const targetWidth = skillBar.clientWidth;
-          skillBar.style.width = "0";
-
-          let width = 0;
-          let interval = setInterval(frame, 1);
-
-          function frame() {
-            if (width >= targetWidth) {
-              clearInterval(interval);
-            } else {
-              width++;
-              skillBar.style.width = width + "px";
-            }
-          }
-        });
-      }
-      // animateSkillBars();
 
     // <!-- ----------Copyright Animation-------- -->
 
@@ -284,80 +231,92 @@ function opentab(tabname) {
       }
     }
 
-   // <!-- ----------end-------- -->
+    //---------------Portfolio Projects Section----------------
 
    const projects = [
     {
       title: 'Notes App',
-      imageUrl: 'images/b-1.jpg',
+      imageUrl: 'images/notesapp.png',
+      websiteUrl: 'https://github.com/BhaskarAcharjee/CorvasNotesApp',
       description: 'The notes app is a versatile tool for organizing and managing digital notes. With features like categorization, search functionality, and cloud synchronization, it provides convenience and accessibility. Whether it\'s jotting down ideas, creating to-do lists, or capturing important information, the notes app is a handy companion for staying organized and productive.',
       category: 'app all'
     },
     {
       title: 'Attendance App',
-      imageUrl: 'images/b-2.jpg',
+      imageUrl: 'images/attandenceapp.png',
+      websiteUrl: '#',
       description: 'The attendance app is a convenient and efficient solution for managing student attendance. It automates the process, allowing for easy tracking and record-keeping. With features like barcode scanning and comprehensive reporting, the app simplifies attendance management, saving time and reducing errors.',
       category: 'app all'
     },
     {
       title: 'Expense Tracker App',
-      imageUrl: 'images/b-3.jpg',
+      imageUrl: 'images/expensetrackerapp.png',
+      websiteUrl: '#',
       description: 'The Expense Tracker app is a powerful tool that helps users efficiently track and manage their personal expenses. With intuitive features like expense categorization, budget management, and data visualization, it provides valuable insights into spending patterns. Stay in control of your finances and make informed decisions with this user-friendly app.',
-      category: 'app'
+      category: 'app all'
     },
     {
       title: 'Pixel Craft',
       imageUrl: 'images/pixelcraft.png',
+      websiteUrl: 'https://github.com/BhaskarAcharjee/PixelCraft',
       description: 'PixelCraft is an image editing project built using the PIL library. With its intuitive interface and a range of features, it allows users to creatively transform their images. From applying filters like blur, grayscale, and sharpen, to using canvas effects, PixelCraft empowers users to unleash their artistic vision.',
       category: 'python all'
     },
     {
       title: 'Sudoku Solver',
       imageUrl: 'images/sudokusolver.png',
+      websiteUrl: 'https://github.com/BhaskarAcharjee/SudokuSolver',
       description: 'A Sudoku solver is an algorithm or program that aims to solve Sudoku puzzles. It takes an incomplete Sudoku grid as input and fills in the missing numbers according to the Sudoku rules. By using logical deductions and backtracking techniques, the solver iterates through the puzzle, making educated guesses and eliminating possibilities until a valid solution is found or all possibilities are exhausted. The Sudoku solver provides a systematic approach to solving Sudoku puzzles efficiently and accurately.',
       category: 'python all'
     },
     {
       title: 'Morse Enigma',
       imageUrl: 'images/morseenigma.png',
+      websiteUrl: 'https://github.com/BhaskarAcharjee/MorseEnigma',
       description: 'Morse Enigma is a communication system that uses Morse code encryption to transmit messages securely. It combines the principles of the traditional Morse code with the complexity of an Enigma machine, adding an additional layer of encryption. The system employs a series of rotating cipher wheels to encode and decode messages, ensuring confidentiality and confidentiality in communication.',
       category: 'python all'
     },
     {
       title: 'AIConverse',
       imageUrl: 'images/aiconverse.png',
+      websiteUrl: 'https://github.com/BhaskarAcharjee/AIConverse',
       description: 'AIConverse is an advanced chatbot platform powered by artificial intelligence. It utilizes natural language processing and machine learning techniques to understand and respond to user queries in a conversational manner. With its intelligent algorithms and customizable features, AIConverse provides a seamless and interactive chatbot experience for various applications, including customer support, virtual assistants, and information retrieval.',
       category: 'python all'
     },
     {
       title: 'HandSphere',
       imageUrl: 'images/handsphere.png',
-      description: 'HandSphere is a unique hand gesture recognition system that combines the capabilities of hand cricket and rock-paper-scissors games. It leverages computer vision and machine learning algorithms to detect and interpret hand gestures in real-time. With HandSphere, users can enjoy interactive and immersive gaming experiences by using hand gestures as input. Whether its playing hand cricket or challenging opponents in rock-paper-scissors, HandSphere brings a new level of fun and engagement to gesture-based gaming.',
+      websiteUrl: 'https://github.com/BhaskarAcharjee/HandSphere',
+      description: 'HandSphere combines the capabilities of hand cricket and rock-paper-scissors games. It leverages computer vision and machine learning algorithms to detect and interpret hand gestures in real-time. With HandSphere, users can enjoy interactive and immersive gaming experiences by using hand gestures as input. Whether its playing hand cricket or challenging opponents in rock-paper-scissors, HandSphere brings a new level of fun and engagement to gesture-based gaming.',
       category: 'python all'
     },    
     {
       title: 'Data Analytics Project',
       imageUrl: 'images/b-4.jpg',
+      websiteUrl: '#',
       description: 'Data Analytics Project description goes here...',
       category: 'data'
     },
     {
-      title: 'Data Analytics Project 2',
-      imageUrl: 'images/b-2.jpg',
-      description: 'Data Analytics Project 2 description goes here...',
-      category: 'data'
+      title: 'Carousel Instagram Post',
+      imageUrl: 'images/studymonkinstaad.png',
+      websiteUrl: 'https://www.behance.net/gallery/174824905/Carousel-Instagram-Post-for-StudyMonk/modules/987244739',
+      description: 'Carousel Instagram Post for StudyMonk',
+      category: 'graphic'
     },
     {
-      title: 'Data Analytics Project 3',
-      imageUrl: 'images/b-3.jpg',
-      description: 'Data Analytics Project 3 description goes here...',
-      category: 'data'
+      title: 'NRR Calculator',
+      imageUrl: 'images/nrrcalculator.png',
+      websiteUrl: 'https://bhaskaracharjee.github.io/NRR-Calculator/',
+      description: 'Net Run Rate (NRR) in cricket is a statistical method of analysing and compare the performance of a team usually during a multi-team tournament. The Net Run Rate plays a critical role to decide which team qualifies for the next round when two or more teams end up with the same number of points.',
+      category: 'all'
     },
     {
       title: 'Poster Design',
       imageUrl: 'images/Museum Poster.png',
+      websiteUrl: 'https://www.behance.net/gallery/174268297/Poster-Design-for-College-Museum/modules/984020541',
       description: 'Poster Design for College Museum description goes here...',
-      category: 'graphic all'
+      category: 'graphic'
     },
     // Add more project here
   ];  
@@ -365,7 +324,7 @@ function opentab(tabname) {
   
   function generateProjectMarkup(project) {
     return `
-      <a href="#" class="portfolio-box" data-lightbox="work">
+      <a href="${project.websiteUrl}" class="portfolio-box" data-lightbox="work">
         <img src="${project.imageUrl}" alt="" />
         <h2 class="project-title">${project.title}</h2>
         <div class="overlay">
@@ -417,4 +376,4 @@ function opentab(tabname) {
     });
   }
   
-  
+  // <!-- ----------end-------- -->
